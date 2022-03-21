c9d5d12111d69cf7860bdafbf66726b02d4dbec9
Bianca Blakesley <bianca.blakesley@gmail.com>
2022-03-21 18:36:35 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Menu Items
+
+## Breakfast Sampler
+'Breakfast Sampler
+'BK001'
+'With our sampler, two is the magic number!'
+['two eggs', 'two hickory-smoked bacon strips', 'two pork sausage links', 'two pieces of ham', 'two fluffy buttermilk pancakes', 'golden hash browns']
+1090
+10.99
+
+## Split Decision
+'Split Decision'
+'BK002'
+'Our hearty combo!'
+['two eggs', 'two crispy strips of hickory-smoked bacon', 'two pork sausage links', 'two French toast triangles,', 'two buttermilk pancakes']
+1120
+12.99
+
+## 2 x 2 x 2
+'2 x 2 x 2'
+'BK003'
+'For our healthy eaters!'
+['two eggs', 'two signature buttermilk pancakes', 'two crispy strips of hickory-smoked bacon', 'two savory pork sausage links']
+1090
+14.99
---
+++ scripts/scripts.js
@@ -0,0 +1,60 @@
+// PROBLEM 1. Create a constructor function called MenuItem. There should be 6 parameters: name, refId, description, ingredients, calories, price. Make sure to include the properties (key/ values) for this constructor.
+
+
+// PROBLEM 2. Add a method to the Hotel constructor called addIngredient. This method will take a string and push it into the ingredients property.
+
+
+// PROBLEM 3. Add method to the Hotel constructor called numIngredients. This method will return the number of ingredients per menu item.
+
+
+// PROBLEM 4. Create 3 objects (breakfastSampler, splitDecision, twoByTwo) using the Hotel constructor function. You will find the information needed in the README.md file to create these objects.
+
+
+
+// PROBLEM 5. Console.log the names of all 3  menu items
+
+console.log("-------------\n");
+console.log("PROBLEM 5. Menu Item Names");
+
+
+
+// PROBLEM 6. Add up the price for all 3 menu items and console.log the total
+
+console.log("-------------\n");
+console.log("PROBLEM 6. Menu Item Total Price");
+
+
+
+// PROBLEM 7. Add hash browns to all 3 menu items
+
+
+// PROBLEM 8. Console.log the total of ingredients for all 3 menu items
+
+console.log("-------------\n");
+console.log("PROBLEM 8. Number of Breakfast Sampler Ingredients");
+
+
+console.log("PROBLEM 8. Number of Split Decision Ingredients");
+
+
+console.log("PROBLEM 8. Number of 2 x 2 x 2 Ingredients");
+
+
+
+// PROBLEM 9. List of all the ingredients per menu item
+
+console.log("-------------\n");
+console.log("PROBLEM 9. List Breakfast Sampler Ingredients");
+
+
+console.log("PROBLEM 9. List Split Decision Ingredients");
+
+
+console.log("PROBLEM 9. List 2 x 2 x 2 Ingredients");
+
+
+
+console.log("-------------\n");
+console.log("PROBLEM 10. Your own method for calories");
+
+// PROBLEM 10. Make your own method for the Hotel constructor that will add up all of the calories for all 3 menu items and return the total. Then console.log the total.
